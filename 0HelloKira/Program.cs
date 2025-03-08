368be55f88e1e65b522dcd5cc2faee0894d637de
kira <113971943+apurvisnotcool@users.noreply.github.com>
2025-03-08 21:01:44 +0530

    Add files via upload

---
+++ 0HelloKira/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, Kira!"); //Print Statement
+//Alright so we are going to learn how to declare a variable
+
+string Friend;
+
+Friend = "Niel"; //Assignment of the name of the friend
+
+System.Console.WriteLine(Friend);
+
+Friend = "Kundan"; //Overwritting the variable with a new Input in this case my new friends name is 
+//kundan
+
+System.Console.WriteLine(Friend);
+
+Console.ReadKey(); //This line makes sure that the program keeps running
+
